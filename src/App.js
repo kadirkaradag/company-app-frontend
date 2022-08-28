7a8197d9be632741a2433af0046463571284cfcc
kadirkaradag <kadirkaradag0132@gmail.com>
2022-08-28 17:32:57 +0300

    company app frontend init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import { BrowserRouter, Route, Routes, NavLink } from "react-router-dom";
+import Department from "./components/Department.jsx";
+import Employee from "./components/Employee.jsx";
+import Home from "./components/Home.jsx";
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App container">
+        <h3 className="d-flex justify-content-center m-3">Company Frontend</h3>
+        <nav className="navbar navbar-expand-sm bg-light navbar-dark">
+          <ul className="navbar-nav">
+            <li className="nav-item-m1">
+              <NavLink className="btn btn-light btn-outline-primary" to="/home">
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-item-m1">
+              <NavLink
+                className="btn btn-light btn-outline-primary"
+                to="/department"
+              >
+                Department
+              </NavLink>
+            </li>
+            <li className="nav-item-m1">
+              <NavLink
+                className="btn btn-light btn-outline-primary"
+                to="/employee"
+              >
+                Employee
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/home" element={<Home></Home>} />
+          <Route path="/department" element={<Department></Department>} />
+          <Route path="/employee" element={<Employee></Employee>} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
---
+++ src/components/Employee.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+
+import React from "react";
+
+const Employee = () => {
+  return (
+    <div>
+      <h3 className="d-flex justify-content-center m-3">Employee Frontend</h3>
+    </div>
+  );
+};
+
+export default Employee;
